86474558f0b1dccf9fdc33da9554cb574f88119b
Matt Belland <kristopher.belland@students.olin.edu>
2011-11-10 14:52:28 -0500

    Combined VerilogSimulator.py into testOrgs.py

--- TestCode/andTest.v
+++ TestCode/andTest.v
@@ -1,27 +1,4 @@
-module andTest;
-	
-    reg A,B;
-    wire out;
-    
-    parameter settleDelay = 10000;
-    
-    trivialAnd uut (out,A,B);
-    
-    integer i;
-    
-    initial
-    begin
-        $display("out,in,in");
-        for (i = 0; i < 4; i = i + 1) begin
-            {A,B} = i;
-            #settleDelay
-            $display ("%d,%d,%d",A,B,out);
-        end
-    end
-    
-endmodule
-
-module trivialAnd(out,A,B);
+module andTest(out,A,B);
 
     output out;
     input A,B;
