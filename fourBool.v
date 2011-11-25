2fec5df60d2cc718bfaecb04175ffd0515211a63
smoon <smoon@ubuntu.(none)>
2011-11-24 22:19:29 -0500

    what conflict

--- fourBool.v
+++ fourBool.v
@@ -6,8 +6,8 @@ module fourBool(output0,output1,output2,output3,input0,input1,input2,input3);
 	wire output0,output1,output2,output3;
 
 	buf #50 (output0,input1);
-	or #50 (output1,input3,input0);
-	nand #50 (output2,input2,input1);
-	not #50 (output3,input0);
+	or #50 (output1,input0,input3);
+	nand #50 (output2,input1,input2);
+	not #50 (output3,input1);
 
 endmodule
