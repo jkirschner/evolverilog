ce33f9d72464770dec4a03710c77d7555a1315ce
Paul Booth <thepaulbooth@gmail.com>
2011-11-30 14:50:24 -0500

    working and slow. Needs variability to work, means large population

--- Winner.v
+++ Winner.v
@@ -1,13 +1,35 @@
-module f(output0,output1,output2,output3,input0,input1,input2,input3);
+module fourBool(output0,output1,output2,output3,input0,input1,input2,input3);
 
 	output output0,output1,output2,output3;
 	input input0,input1,input2,input3;
+	buf #50 (output0_branch01010,input2);
+	or #50 (output0_branch0101,output0_branch01010,input1);
+	or #50 (output0_branch010,input0,output0_branch0101);
+	buf #50 (output0_branch0110,input1);
+	or #50 (output0_branch0111,input3,input2);
+	nand #50 (output0_branch011,output0_branch0110,output0_branch0111);
+	nand #50 (output0_branch01,output0_branch010,output0_branch011);
+	and #50 (output0,input2,output0_branch01);
 
-	wire output0,output1,output2,output3;
+	and #50 (output1_branch01000,input2,input0);
+	not #50 (output1_branch0100,output1_branch01000);
+	and #50 (output1_branch0101,input2,input0);
+	nand #50 (output1_branch010,output1_branch0100,output1_branch0101);
+	not #50 (output1_branch01,output1_branch010);
+	xor #50 (output1,input1,output1_branch01);
 
-	and #50 (output0,input2,input1);
-	buf #50 (output1,input1);
-	nand #50 (output2,input0,input1);
-	xor #50 (output3,input2,input3);
+	xor #50 (output2_branch000000,input0,input2);
+	or #50 (output2_branch000001,input2,input1);
+	or #50 (output2_branch00000,output2_branch000000,output2_branch000001);
+	not #50 (output2_branch0000,output2_branch00000);
+	or #50 (output2_branch0001,input2,input1);
+	and #50 (output2_branch000,output2_branch0000,output2_branch0001);
+	xor #50 (output2_branch00,output2_branch000,input1);
+	nand #50 (output2,output2_branch00,input0);
+
+	buf #50 (output3_branch010,input2);
+	buf #50 (output3_branch01,output3_branch010);
+	xor #50 (output3,input3,output3_branch01);
+
 
 endmodule
--- fourBool.v
+++ fourBool.v
@@ -2,28 +2,42 @@ module fourBool(output0,output1,output2,output3,input0,input1,input2,input3);
 
 	output output0,output1,output2,output3;
 	input input0,input1,input2,input3;
-	or #50 (output0_branch0010,input0,input3);
-	not #50 (output0_branch001,output0_branch0010);
-	nand #50 (output0_branch00,input2,output0_branch001);
-	not #50 (output0,output0_branch00);
+	nand #50 (output0_branch0000100,input0,input0);
+	buf #50 (output0_branch000010,output0_branch0000100);
+	xor #50 (output0_branch00001100,input3,input3);
+	buf #50 (output0_branch0000110,output0_branch00001100);
+	and #50 (output0_branch000011,output0_branch0000110,input1);
+	and #50 (output0_branch00001,output0_branch000010,output0_branch000011);
+	nand #50 (output0_branch0000,input0,output0_branch00001);
+	or #50 (output0_branch000,output0_branch0000,input1);
+	and #50 (output0_branch0010,input0,input2);
+	or #50 (output0_branch001,output0_branch0010,input3);
+	and #50 (output0_branch00,output0_branch000,output0_branch001);
+	not #50 (output0_branch0101,input0);
+	xor #50 (output0_branch010,input1,output0_branch0101);
+	xor #50 (output0_branch01,output0_branch010,input1);
+	nand #50 (output0,output0_branch00,output0_branch01);
 
-	buf #50 (output1,input1);
+	not #50 (output1_branch00,input2);
+	not #50 (output1,output1_branch00);
 
-	not #50 (output2_branch00,input2);
-	or #50 (output2,output2_branch00,input2);
+	not #50 (output2_branch0000,input0);
+	or #50 (output2_branch0001100,input2,input1);
+	or #50 (output2_branch000110,output2_branch0001100,input0);
+	not #50 (output2_branch00011,output2_branch000110);
+	or #50 (output2_branch0001,input1,output2_branch00011);
+	and #50 (output2_branch000,output2_branch0000,output2_branch0001);
+	xor #50 (output2_branch00,output2_branch000,input1);
+	nand #50 (output2,output2_branch00,input0);
 
-	or #50 (output3_branch0000,input3,input2);
-	buf #50 (output3_branch0001010,input1);
-	or #50 (output3_branch000101,output3_branch0001010,input0);
-	xor #50 (output3_branch00010,input3,output3_branch000101);
-	xor #50 (output3_branch0001,output3_branch00010,input0);
-	and #50 (output3_branch000,output3_branch0000,output3_branch0001);
-	buf #50 (output3_branch00,output3_branch000);
-	not #50 (output3_branch0100,input1);
-	buf #50 (output3_branch0101,input3);
-	nand #50 (output3_branch010,output3_branch0100,output3_branch0101);
-	nand #50 (output3_branch01,output3_branch010,input0);
-	and #50 (output3,output3_branch00,output3_branch01);
+	buf #50 (output3_branch0001,input3);
+	or #50 (output3_branch000,input0,output3_branch0001);
+	not #50 (output3_branch0010,input2);
+	buf #50 (output3_branch00111,input1);
+	or #50 (output3_branch0011,input0,output3_branch00111);
+	or #50 (output3_branch001,output3_branch0010,output3_branch0011);
+	nand #50 (output3_branch00,output3_branch000,output3_branch001);
+	buf #50 (output3,output3_branch00);
 
 
 endmodule
